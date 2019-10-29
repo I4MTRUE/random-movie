7753958beacfbb35cc3221f0feadfa9cab786e50
Emeric Baha <emericbaha@gmail.com>
2019-10-29 14:57:09 +0100

    v. 1.0 enfin

--- javascript.js
+++ javascript.js
@@ -5,48 +5,38 @@ function pad(number, length) {
   }
  return str;
 }
-var i = 0
-var votes = 0
-var a = 0
-var fuk = 0
-votes < 10
-var movie = pad(Math.floor((Math.random() * 2155529) + 1), 7);
-yeet(movie);
-function yeet(movie,callback){
-$.getJSON("http://www.omdbapi.com/?i=tt"+movie+"&apikey=bf57cc7e", function(data){
-  var title = data.Title;
-  var year = data.Year;
-  var rating = data.imdbRating;
-  var actors = data.Actors;
-  var plot = data.Plot;
-  var season = data.Season;
-  var imdbVotes = data.imdbVotes; //les votes ne marchent pas :(
-  var votes = parseInt(imdbVotes,10);
-  var type = data.Type;
-  var link = 'http://www.imdb.com/title/tt'+movie+'/';
-  var debug = 'http://www.omdbapi.com/?i=tt'+movie+'&apikey=bf57cc7e'
+  var movie = pad(Math.floor((Math.random() * 2155529) + 1), 7);
+  yeet(movie);
+function yeet(movie){
+  $.getJSON("http://www.omdbapi.com/?i=tt"+movie+"&apikey=525ac35b", function(data) {
+    var title = data.Title;
+    var year = data.Year;
+    var rating = data.imdbRating;
+    var actors = data.Actors;
+    var plot = data.Plot;
+    var season = data.Season;
+    var imdbVotes = data.imdbVotes ;
+    var votes = parseInt(imdbVotes,10);
+    var type = data.Type;
+    var link = 'http://www.imdb.com/title/tt'+movie+'/';
+    var debug = 'http://www.omdbapi.com/?i=tt'+movie+'&apikey=525ac35b'
 
-  votes = votes || 0;
-        if (votes == "N/A") {votes = 0;}
-        else {votes = votes;};
-  votes = parseInt(votes);
-  if (votes > 50){fuk = 1
-  console.log(fuk);
-  fuk = parseInt(fuk);};
-  i = i + 1
-if(fuk == 0){
-console.log("here we go again")
-var movie = pad(Math.floor((Math.random() * 2155529) + 1), 7);
-yeet(movie);
-}
-else{
-$("#title").text(title);
-$("#type").text(type);
-$("#year").text(year);
-$("#rating").text(rating);
-$("#actors").text(actors);
-$("#plot").text(plot);
-$("#movie").text(movie);
-$("#season").text(season);
-$("#debug").text(debug);
-$("#link").attr("href", link);}})}
+    if (votes > 500) {
+    console.log(votes);
+    $("#title").text(title);
+    $("#type").text(type);
+    $("#year").text(year);
+    $("#rating").text(rating);
+    $("#actors").text(actors);
+    $("#plot").text(plot);
+    $("#movie").text(movie);
+    $("#season").text(season);
+    $("#debug").text(debug);
+    $("#link").attr("href", link);
+                        }
+    else {
+    movie = pad(Math.floor((Math.random() * 2155529) + 1), 7);
+    console.log("here we go again")
+    yeet(movie);
+    }
+  })}
