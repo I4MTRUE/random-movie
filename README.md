e9cb3572784eb4d627ee45d5eb91b8e8108e1ed6
Emeric Baha <emericbaha@gmail.com>
2019-10-29 15:00:59 +0100

    correction

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Random-Movie
 
 http://www.random-movie.ml/
+
+API utilisé ombdapi avec clé : bf57cc7e (clé payante 100 000 requêtes)
--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function pad(number, length) {
   var movie = pad(Math.floor((Math.random() * 2155529) + 1), 7);
   yeet(movie);
 function yeet(movie){
-  $.getJSON("http://www.omdbapi.com/?i=tt"+movie+"&apikey=525ac35b", function(data) {
+  $.getJSON("http://www.omdbapi.com/?i=tt"+movie+"&apikey=bf57cc7e", function(data) {
     var title = data.Title;
     var year = data.Year;
     var rating = data.imdbRating;
@@ -19,7 +19,7 @@ function yeet(movie){
     var votes = parseInt(imdbVotes,10);
     var type = data.Type;
     var link = 'http://www.imdb.com/title/tt'+movie+'/';
-    var debug = 'http://www.omdbapi.com/?i=tt'+movie+'&apikey=525ac35b'
+    var debug = 'http://www.omdbapi.com/?i=tt'+movie+'&apikey=bf57cc7e'
 
     if (votes > 500) {
     console.log(votes);
